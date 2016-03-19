3ebd764d1095ce68fb7dd2ff8c5cd1c7fa52d98c
Michael Park <migrap@outlook.com>
2016-03-19 15:44:37 -0700

    Removed unused component

--- src/sam-typescript-angular2-aspnet/wwwroot/app/main.ts
+++ src/sam-typescript-angular2-aspnet/wwwroot/app/main.ts
@@ -1,6 +1,5 @@
 ﻿import { /* enableProdMode,*/ provide } from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-//import {AppComponent} from './app.component';
 import {RocketComponent} from './rocket/rocket.component';
 
 bootstrap(RocketComponent)

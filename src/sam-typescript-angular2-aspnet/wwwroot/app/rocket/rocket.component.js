d43fb0a59c92c579cf5fca8a2f9ea5cf7a963240
Michael Park <migrap@outlook.com>
2016-03-21 08:25:35 -0700

    Added comments about further work/improvements

--- src/sam-typescript-angular2-aspnet/wwwroot/app/rocket/rocket.component.js
+++ src/sam-typescript-angular2-aspnet/wwwroot/app/rocket/rocket.component.js
@@ -42,6 +42,9 @@ System.register(['angular2/core', './rocket.actions', './rocket.model', './rocke
                 }
                 ngOnInit() {
                     this.views.updated.subscribe((representation) => {
+                        // Not sure how efficient this is at replacing the DOM. Gut tells me it's exspensive.
+                        // Could create a single view/template with ngIfs and a ViewModel that it would bind to.
+                        // ViewModel properties would updated based on state
                         this.loader.loadIntoLocation(representation, this.elementRef, 'rocket').then((component) => {
                             if (this.component) {
                                 this.component.dispose();
--- src/sam-typescript-angular2-aspnet/wwwroot/app/rocket/rocket.component.ts
+++ src/sam-typescript-angular2-aspnet/wwwroot/app/rocket/rocket.component.ts
@@ -22,6 +22,9 @@ export class RocketComponent extends Sam<RocketActions, RocketModel, RocketState
 
     ngOnInit() {
         this.views.updated.subscribe((representation) => {
+            // Not sure how efficient this is at replacing the DOM. Gut tells me it's exspensive.
+            // Could create a single view/template with ngIfs and a ViewModel that it would bind to.
+            // ViewModel properties would updated based on state
             this.loader.loadIntoLocation(representation, this.elementRef, 'rocket').then((component) => {
                 if (this.component) {
                     this.component.dispose();
